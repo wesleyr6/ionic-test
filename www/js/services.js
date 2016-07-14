db620a4e0bc4041c45bb16cf1168e2912bb17d73
Wesley Amaro <maz_wamaro@uolinc.com>
2016-07-14 15:56:07 -0300

    Creating services

--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,72 @@
-angular.module('app.services', [])
+angular.module('starter.services', [])
 
-.factory('BlankFactory', [function(){
+.factory('Chats', function() {
+	var chats = [{
+		id: 0,
+		name: 'Ytalo dos Santos',
+		lastText: 'Batman Arkan City',
+		face: 'img/ben.png'
+	}, {
+		id: 1,
+		name: 'Bruno Nascimento',
+		lastText: 'Batman Arkan City',
+		face: 'img/max.png'
+	}, {
+		id: 2,
+		name: 'Adriano Alves',
+		lastText: 'Volkswagem Fusca',
+		face: 'img/adam.jpg'
+	}, {
+		id: 3,
+		name: 'Thiago Paulino',
+		lastText: 'Volkswagem Fusca',
+		face: 'img/perry.png'
+	}, {
+		id: 4,
+		name: 'José Damiani',
+		lastText: 'Batman Arkan City',
+		face: 'img/mike.png'
+	}];
 
-}])
+	return {
+		all: function() {
+			return chats;
+		},
+		remove: function(chat) {
+			chats.splice(chats.indexOf(chat), 1);
+		},
+		get: function(chatId) {
+			for (var i = 0; i < chats.length; i++) {
+				if (chats[i].id === parseInt(chatId)) {
+					return chats[i];
+				}
+			}
+			return null;
+		}
+	};
+})
 
-.service('BlankService', [function(){
+.factory('Account', function() {
+	var accountItems = [{
+		id: 0,
+		name: 'Batman Arkan City',
+		category: 'Games',
+		description: 'Xbox',
+		photo: 'img/ben.png'
+	}, {
+		id: 1,
+		name: 'Volkswagem Fusca',
+		category: 'Collectibles',
+		description: 'Miniatura',
+		photo: 'img/max.png'
+	}];
 
-}]);
-
+	return {
+		all: function() {
+			return accountItems;
+		},
+		remove: function(chat) {
+			accountItems.splice(accountItems.indexOf(chat), 1);
+		}
+	};
+});
