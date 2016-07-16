29c182563fe4d4c48f879ac447d93cb7b628b6aa
Wesley Amaro <contato@wesleyamaro.com>
2016-07-16 16:43:31 -0300

    Config routes

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,4 @@
 // Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
@@ -31,52 +25,70 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 	// setup an abstract state for the tabs directive
     .state('tab', {
-	url: '/tab',
-	abstract: true,
-	templateUrl: 'templates/tabs.html'
-  })
+		url: '/tab',
+		templateUrl: 'templates/tabs.html'
+	})
 
-  // Each tab has its own nav history stack:
-
-  .state('tab.home', {
-	url: '/home',
-	views: {
-		'tab-home': {
-			templateUrl: 'templates/tab-home.html',
-			controller: 'MatchCtrl'
+	.state('tab.games', {
+		url: '/games',
+		views: {
+			'tab-games': {
+				templateUrl: 'templates/tab-games.html',
+				controller: 'GamesCtrl'
+			}
 		}
-	}
-  })
+	})
 
-  .state('tab.chats', {
-	url: '/chats',
-	views: {
-		'tab-chats': {
-			templateUrl: 'templates/tab-chats.html',
-			controller: 'ChatsCtrl'
+	.state('tab.games2', {
+		url: '/games',
+		views: {
+			'tab-games': {
+				templateUrl: 'templates/tab-games2.html',
+				controller: 'GamesCtrl'
+			}
 		}
-	}
+	})
+
+	.state('tab.collectibles', {
+		url: '/collectibles/:id',
+		views: {
+			'tab-collectibles': {
+				templateUrl: 'templates/tab-collectibles.html',
+				controller: 'CollectiblesCtrl'
+			}
+		}
+	})
+
+	.state('tab.chats', {
+		url: '/chats',
+		views: {
+			'tab-chats': {
+				templateUrl: 'templates/tab-chats.html',
+				controller: 'ChatsCtrl'
+			}
+		}
     })
+
     .state('tab.chat-detail', {
-	url: '/chats/:chatId',
-	views: {
-		'tab-chats': {
-			templateUrl: 'templates/chat-detail.html',
-			controller: 'ChatDetailCtrl'
+		url: '/chats/:chatId',
+		views: {
+			'tab-chats': {
+				templateUrl: 'templates/chat-detail.html',
+				controller: 'ChatDetailCtrl'
+			}
 		}
-	}
     })
 
-  .state('tab.account', {
-	url: '/account',
-	views: {
-		'tab-account': {
-			templateUrl: 'templates/tab-account.html',
-			controller: 'AccountCtrl'
+	.state('tab.account', {
+		url: '/account',
+		views: {
+			'tab-account': {
+				templateUrl: 'templates/tab-account.html',
+				controller: 'AccountCtrl'
+			}
 		}
-	}
-  });
+  	});
 
 	// if none of the above states are matched, use this as the fallback
-	$urlRouterProvider.otherwise('/tab/home');
+	$urlRouterProvider.otherwise('/tab/games');
 });
